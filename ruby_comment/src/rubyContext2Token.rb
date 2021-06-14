184cf89adfa1c3f29db1212bf283f03948faac24
白石誠 <makotinator.15@gmail.com>
2021-06-14 22:58:20 +0900

    added if the name globbed is a file

--- ruby_comment/src/rubyContext2Token.rb
+++ ruby_comment/src/rubyContext2Token.rb
@@ -18,53 +18,54 @@ Dir.glob("../repositories/*") do |i|
 
   #各ファイルのループ
   Dir.glob("#{i}/**/*.rb") do |file|
-    token_appear = {}
-    #ファイル名をrep変数に格納
-    rep = file.split('/')[3..-1].join('_')
-    puts file
+    if File.file?(file)
+      token_appear = {}
+      #ファイル名をrep変数に格納
+      rep = file.split('/')[3..-1].join('_')
+      puts file
 
-    contents = File.read(file)
-    lex = Ripper.lex(contents)
-    lex = lex.select do |l|
-      l[1] != :on_sp
-    end
-    tokens = []
-    lex.each do |l|
-      if l[1] == :on_ident
-        tokens << (l[2].to_s).split(' ').join('_').downcase
-      elsif l[1] == :on_kw
-        tokens << l[2].to_s.downcase
-      elsif l[1] != :on_comment
-        tokens << l[1].to_s.downcase
+      contents = File.read(file)
+      lex = Ripper.lex(contents)
+      lex = lex.select do |l|
+        l[1] != :on_sp
       end
-    end
-
-    tokens.each do |token|
-      temp_tok = token
-      if token_appear[temp_tok] == nil
-        token_appear[temp_tok] = 1
-      else
-        token_appear[temp_tok] += 1
+      tokens = []
+      lex.each do |l|
+        if l[1] == :on_ident
+          tokens << (l[2].to_s).split(' ').join('_').downcase
+        elsif l[1] == :on_kw
+          tokens << l[2].to_s.downcase
+        elsif l[1] != :on_comment
+          tokens << l[1].to_s.downcase
+        end
       end
-    end
 
-    tokens.size.times do |i|
-      if token_appear[tokens[i]] <= minAppear2UNK
-        tokens[i] = "UNK"
+      tokens.each do |token|
+        temp_tok = token
+        if token_appear[temp_tok] == nil
+          token_appear[temp_tok] = 1
+        else
+          token_appear[temp_tok] += 1
+        end
       end
+
+      tokens.size.times do |i|
+        if token_appear[tokens[i]] <= minAppear2UNK
+          tokens[i] = "UNK"
+        end
+      end
+
+      File.open("#{root_folder_name}/#{folder_name}/all.txt",mode="a"){ |f|
+        f.puts tokens.join(" ")
+      }
+      File.open("#{root_folder_name}/all.txt",mode="a"){ |f|
+        f.puts tokens.join(" ")
+      }
+
+      File.open("#{root_folder_name}/#{folder_name}/all.txt",mode="a"){ |f|
+        f.puts (["EMP"]*sizeOfContext).join(' ')
+      }
     end
-
-    File.open("#{root_folder_name}/#{folder_name}/all.txt",mode="a"){ |f|
-      f.puts tokens.join(" ")
-    }
-    File.open("#{root_folder_name}/all.txt",mode="a"){ |f|
-      f.puts tokens.join(" ")
-    }
-
-    File.open("#{root_folder_name}/#{folder_name}/all.txt",mode="a"){ |f|
-      f.puts (["EMP"]*sizeOfContext).join(' ')
-    }
-
   end
 end
 
