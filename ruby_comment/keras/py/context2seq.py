fa6677f804c88593ebc3614c07aefd5fb61d3872
白石誠 <makotinator.15@gmail.com>
2021-07-14 15:06:41 +0900

    modified python files

--- ruby_comment/keras/py/context2seq.py
+++ ruby_comment/keras/py/context2seq.py
@@ -10,19 +10,28 @@ import random
 import sys
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, LSTM
-from keras.optimizers import RMSprop
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, LSTM
+from tensorflow.keras.optimizers import RMSprop
 
 sys.path.append('..')
 sys.path.append('../..')
 
 #単語の分散表現のベクトルの次元数
-vecSize = 30
+vecSize = 100
 tokenSize = 20
+
+#コンテキストのサイズ
+contextSize = 20
 
 model = Word2Vec.load("/home/u00545/comments/RubyCommentsML/ruby_comment/models/context2Vec.model")
 file = glob.glob("/home/u00545/comments/RubyCommentsML/ruby_comment/repositories_cleansing/repositories2TokenWithCommentUpDown20Tokens/all.txt")
+
+print(len(model.wv.vocab))
+
+print(model.wv.vocab['require'].__dict__)
+
+print(model.wv.vocab)
 
 
 context = []
@@ -30,12 +39,13 @@ f = open(file[0])
 lines = f.readlines()
 for line in lines:
     temp_data = line.replace('\n','')
-    temp_data = temp_data.split(' ')[0:tokenSize]
+    temp_data = temp_data.split(' ')[0:contextSize]
     context.append(temp_data)
 f.close()
 print(len(context))
 
-print(context[0])
+print(context[1])
+print(len(context[0]))
 print(context[0][0])
 
 #テキストのベクトル化
@@ -75,7 +85,7 @@ next_chars = np.array(next_chars)
 # モデルを定義する
 
 models = Sequential()
-models.add(LSTM(128,input_shape=(maxLen,vecSize)))
+models.add(LSTM(128,batch_input_shape=(None,maxLen,vecSize)))
 
 models.add(Dense(vecSize))
 
@@ -91,15 +101,19 @@ models.compile(loss='mean_squared_error', optimizer=optimizer)
 def sample(preds):
     return model.most_similar( [ np.array(preds) ], [], 5)[0][0]
 
-for iteration in range(1,50):
+for iteration in range(1,30):
     print()
     print('-'*50)
     print('繰り返し回数: ', iteration)
-    models.fit(sentences,next_chars,batch_size=128,epochs=3)
+    models.fit(np.array(sentences),np.array(next_chars),epochs=2)
 
-    start_index = random.randint(0, len(context)-maxLen-1)
+    start_index = random.randint(0, len(sentences)-1)
+    while model.most_similar([sentences[start_index][0] ],[],1)[0][0] == "UNK":
+        start_index = random.randint(0, len(sentences))
 
     generated = ''
+    #ベクトル表現から単語を得ている。
+    #初期値となる単語を得ている。
     sentence = model.most_similar([sentences[start_index][0] ],[],1)[0][0]
     generated += sentence
     print('-----Seedを生成しました"' + sentence + '"')
@@ -107,28 +121,28 @@ for iteration in range(1,50):
     sys.stdout.write(generated)
 
     x = sentences[start_index]
-    print(x)
     for i in range(400):
         input_x = x[-maxLen:]
         input_x = np.array(input_x)
-        input_x = np.array([input_x])
-        preds = models.predict([input_x],verbose=2)[0]
+        #形を(1,maxLen,textSize)にするため
+        input_x = input_x.reshape(1,input_x.shape[0],input_x.shape[1])
+        #次の単語の分散表現を得ている。
+        preds = models.predict(input_x,verbose=2)[0]
+        
         next_char = sample(preds)
         generated += next_char
         generated += " "
-        sentence = sentence[1:] + next_char
         x = np.append(x, np.array([model.wv[next_char]]),axis=0)
-
-        sys.stdout.write(next_char)
-        sys.stdout.write(" ")
-        sys.stdout.flush()
+    print()
+    print(generated)
     print()
     
-models.save("context2SeqUpDown20Tokens.model")
+    
+models.save("../../models/context2SeqUpDown20Tokens.model")
 
 #ここからはmodelをloadしてきて遊んでみる
 
-models = keras.models.load_model("context2SeqUpDown20Tokens.model")
+models = keras.models.load_model("../../models/context2SeqUpDown20Tokens.model")
 print(np.array([context2Vec[0][:maxLen]]).shape)
 
 preds = models.predict(np.array([context2Vec[1][:maxLen]]),verbose=2)
--- ruby_comment/keras/py/contextWord2Vec.py
+++ ruby_comment/keras/py/contextWord2Vec.py
@@ -14,7 +14,9 @@ sys.path.append('../..')
 file = glob.glob("/home/u00545/comments/RubyCommentsML/ruby_comment/repositories_cleansing/repositories2Token/all.txt")
 print(file)
 
-vectorSize = 30
+#単語の分散表現のベクトルの次元数
+vecSize = 100
+minCount = 3
 windowSize = 20
 
 context = []
@@ -30,10 +32,42 @@ print(len(context))
 
 print(context[0:10])
 
+#文字の数を出力
+chars = sorted(list(set(context)))
+print(len(chars))
+print(context[0:1000])
 
-model = Word2Vec(sentences=[context], size=vectorSize,window=windowSize,iter=60)
-model.save("context2Vec.model")
+
+model = Word2Vec(sentences=[context], size=vecSize,window=20,iter=60, min_count=minCount)
+model.save("../../models/context2Vec.model")
 print(model.__dict__)
+print("list of words detected")
+print(model.wv.index2word)
+
+print("number of words")
+print(len(model.wv.index2word))
+
+print("most similar word for if")
 print(model.most_similar( positive="if"))
-model.wv.index2word
+print()
+print("most similar word for else")
+print(model.most_similar( positive="else"))
+print()
+print("most similar word for while")
+print(model.most_similar( positive="while"))
+print()
+print("most similar word for def")
+print(model.most_similar( positive="end"))
 print(model.wv["for"])
+
+print("vector of  if")
+print(model.wv["if"])
+print()
+print("vector of  else")
+print(model.wv["else"])
+print()
+print("vector of  while")
+print(model.wv["while"])
+print()
+print("vector of  end")
+print(model.wv["end"])
